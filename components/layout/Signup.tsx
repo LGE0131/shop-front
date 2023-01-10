5a4e5d7d105854417e264df6fe0957923762a7a7
LGE0131 <igoeun0131@gamil.com>
2023-01-10 16:48:45 +0900

    이메일 유효문자인지 검증, 패스워드 비교 확인 검증 구현

--- components/layout/Signup.tsx
+++ components/layout/Signup.tsx
@@ -1,86 +1,171 @@
-import React, { useState, useEffect, useRef } from 'react'
-import styles from '../../styles/css/signup.module.css'
-import { useAppDispatch } from '../../redux/hooks/index'
-import { signup } from '../../redux/slices/userSlice'
+import React, { useState, useEffect, useRef } from "react";
+import styles from "../../styles/css/signup.module.css";
+import { useAppDispatch } from "../../redux/hooks/index";
+import { signup } from "../../redux/slices/userSlice";
 
+// interface ValidationInput {
+//   validUserName: string | undefined;
+//   validUserEmail: string | undefined;
+//   validUserPw: string | undefined;
+//   validUserPwConfirm: string | undefined;
+// }
 
-const Signup = () => {
-
-    // const [input, setInput] = useState({
-    //     userName: '',
-    //     userEmail: '',
-    //     userPw:'',
-    //     userPwConfirm:''
-    // })
-
-    // const {userName, userEmail, userPw, userPwConfirm} = input
-
-    // const [userName, setUserName] = useState<string>('');
-    // const [userEmail, setUserEmail] = useState<string>('');
-    // const [userPw, setUserPw] = useState<string>('');
-    // const [userPwConfirm, setUserPwConfirm] = useState<string>('');
-
-
-    const [pwError, setPwError] = useState<boolean>(false);
-
-    const userNameRef: React.LegacyRef<HTMLInputElement> = useRef(null);
-    const userPwRef: React.LegacyRef<HTMLInputElement> = useRef(null);
-    const userEmailRef: React.LegacyRef<HTMLInputElement> = useRef(null);
-    const userPwConfilrmRef: React.LegacyRef<HTMLInputElement> = useRef(null)
-
-    const dispatch = useAppDispatch();
-     
-    useEffect(() => {
-        if (userPwRef.current?.value !== userPwConfilrmRef.current?.value) {
-            setPwError(true);
-        }
-        return () => {
-          setPwError(false);
-        };
-    }, [userPwRef.current?.value, userPwConfilrmRef.current?.value]);
-
-    const signupSubmit = (e: React.SyntheticEvent) => {
-        e.preventDefault()
-        const input = {
-            userName: userNameRef.current?.value,
-            userEmail: userEmailRef.current?.value,
-            userPw: userPwRef.current?.value,
-            userPwConfilrm: userPwConfilrmRef.current?.value,
-        }
-        console.log(input)
-        dispatch(signup(input))
-    }
-
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.signupFormBox}>
-                <form className={styles.signupForm} onSubmit={signupSubmit} name='orderForm'>
-                    <fieldset className={styles.signupFormFieldset}>
-                        <legend className={styles.signupFormTitle}>회원가입</legend>
-                        <div className={styles.name}>
-                            <input className={styles.nameInput} type='text' name='name' placeholder='이름'
-                                autoComplete='off' ref={userNameRef} />
-                        </div>
-                        <div className={styles.email}>
-                            <input className={styles.emailInput} type='text' name='email' placeholder='이메일'
-                                autoComplete='off' ref={userEmailRef} />
-                        </div>
-                        <div className={styles.Pw}>
-                            <input className={styles.pwInput} type='password' name='password' placeholder='비밀번호'
-                                autoComplete='off' ref={userPwRef} />
-                        </div>
-                        <div className={styles.PwConfirm}>
-                            <input className={styles.PwConfirmInput} type='password' name='passwordConfirm'
-                                placeholder='비밀번호 확인' autoComplete='off' ref={userPwConfilrmRef} />
-                        </div>
-                        {pwError && (<span>비밀번호가 일치하지 않습니다.</span>)}
-                        <input className={styles.signupDone} type='submit' value='가입완료!' />
-                    </fieldset>
-                </form>
-            </div>
-        </div>
-    )
+interface Input {
+  userName: string | undefined;
+  userEmail: string | undefined;
+  userPw: string | undefined;
+  userPwConfirm: string | undefined;
 }
 
-export default Signup
+// interface FrontSignupValidation {
+//   isEmail: boolean;
+//   isPw: boolean;
+//   isName: boolean;
+//   disabledSignupBtn: boolean;
+// }
+
+const Signup = () => {
+  const [validationInput, setValidationInput] = useState({
+    // validUserName: "",
+    // validUserEmail: "",
+    // validUserPw: "",
+    // validUserPwConfirm: "",
+  });
+
+  const [pwSame, setPwSame] = useState<boolean>(false);
+  const [isEmail, setIsEmail] = useState<boolean>(false);
+  const [isPw, setIsPw] = useState<boolean>(false);
+  const [isName, setIsName] = useState<boolean>(false);
+  const [disabledSignupBtn, setDisabledSignupBtn] = useState<boolean>(false);
+
+  const userNameRef: React.LegacyRef<HTMLInputElement> = useRef(null);
+  const userPwRef: React.LegacyRef<HTMLInputElement> = useRef(null);
+  const userEmailRef: React.LegacyRef<HTMLInputElement> = useRef(null);
+  const userPwConfilrmRef: React.LegacyRef<HTMLInputElement> = useRef(null);
+
+  const input: Input = {
+    userName: userNameRef.current?.value,
+    userEmail: userEmailRef.current?.value,
+    userPw: userPwRef.current?.value,
+    userPwConfirm: userPwConfilrmRef.current?.value,
+  };
+
+  //   const frontSignupValidation: FrontSignupValidation = {
+  //     isEmail: false,
+  //     isPw: false,
+  //     isName: false,
+  //     disabledSignupBtn: false,
+  //   };
+
+  const dispatch = useAppDispatch();
+
+  const handlerChange = (e: any) => {
+    setValidationInput({
+      ...validationInput,
+      [e.target.name]: e.target.value,
+    });
+    console.log(validationInput);
+  };
+
+  useEffect(() => {
+    if (input.userPw === input.userPwConfirm) {
+      setPwSame(true);
+    } else {
+      setPwSame(false);
+    }
+    return () => {
+      setPwSame(true);
+    };
+  }, [input.userPw, input.userPwConfirm]);
+
+  useEffect(() => {
+    let regex = new RegExp(
+      "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+    );
+    if (regex.test(input.userEmail)) {
+      setIsEmail(true);
+    } else {
+      setIsEmail(false);
+    }
+    return () => {
+      setIsEmail(false);
+    };
+  }, [input.userEmail]);
+
+  const signupSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    console.log(input);
+    dispatch(signup(input));
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.signupFormBox}>
+        <form
+          className={styles.signupForm}
+          onSubmit={signupSubmit}
+          name="orderForm"
+        >
+          <fieldset className={styles.signupFormFieldset}>
+            <legend className={styles.signupFormTitle}>회원가입</legend>
+            <div className={styles.name}>
+              <input
+                className={styles.nameInput}
+                type="text"
+                name="name"
+                placeholder="이름 2자 이상"
+                autoComplete="off"
+                ref={userNameRef}
+                onChange={handlerChange}
+              />
+            </div>
+            <div className={styles.email}>
+              <input
+                className={styles.emailInput}
+                type="text"
+                name="email"
+                placeholder="test@email.com"
+                autoComplete="off"
+                ref={userEmailRef}
+                onChange={handlerChange}
+              />
+            </div>
+            {isEmail ? null : <span>유효한 이메일 주소가 아닙니다.</span>}
+            <div className={styles.Pw}>
+              <input
+                className={styles.pwInput}
+                type="password"
+                name="password"
+                placeholder="특수문자, 숫자, 영대소문자 각 3가지 포함 8 ~ 25자"
+                autoComplete="off"
+                ref={userPwRef}
+                onChange={handlerChange}
+              />
+            </div>
+            <div className={styles.PwConfirm}>
+              <input
+                className={styles.PwConfirmInput}
+                type="password"
+                name="passwordConfirm"
+                placeholder="비밀번호 확인"
+                autoComplete="off"
+                ref={userPwConfilrmRef}
+                onChange={handlerChange}
+              />
+            </div>
+            {pwSame ? null : <span>비밀번호가 일치하지 않습니다.</span>}
+            <input
+              className={styles.signupDone}
+              type="submit"
+              value="가입완료!"
+              disabled
+            />
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
